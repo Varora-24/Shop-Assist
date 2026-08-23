12e67e3afee6a542f1b359781a046e4729f98b0d
Varora-24 <vansh.23bai10638@vitbhopal.ac.in>
2026-08-23 22:10:53 +0530

    chore: remove untracked files

--- fix-max-w.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-let suggest = fs.readFileSync('src/components/SuggestionsPanel.tsx', 'utf8');
-suggest = suggest.replace('max-w-md', 'max-w-2xl');
-fs.writeFileSync('src/components/SuggestionsPanel.tsx', suggest);
-
-let search = fs.readFileSync('src/components/SearchPanel.tsx', 'utf8');
-search = search.replace('max-w-md', 'max-w-2xl');
-fs.writeFileSync('src/components/SearchPanel.tsx', search);
